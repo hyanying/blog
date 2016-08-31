6faf58b303d865ce09d358e5832ee8380842556a
hyanying <yan.and.ying@gmail.com>
2016-08-31 16:05:11 +0100

    updated posts index CSS

---
+++ db/migrate/20160831143409_add_image_link_to_posts.rb
@@ -0,0 +1,5 @@
+class AddImageLinkToPosts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831073136) do
+ActiveRecord::Schema.define(version: 20160831143409) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160831073136) do
     t.datetime "dateedited"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image_url"
   end
 
   create_table "users", force: :cascade do |t|
