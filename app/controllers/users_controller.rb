083237937ba2ee7b709e71424f1d194652329ff4
hyanying <yan.and.ying@gmail.com>
2016-09-01 10:43:19 +0100

    updated admin user management pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,40 @@ class UsersController < ApplicationController
   	end	
 
   end	
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_url, notice: 'User was successfully updated.' }
+        #format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
  
 private
  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
   	params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
